46f02b4214d99bd0490982b4d66b85898de4e8af
Rafael Adam <professionalacct.rafaeladam@gmail.com>
2026-01-30 18:33:12 -0300

    Livros são exibidos na tela inicial. Usuários podem acessar a página show do livro

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :require_admin
+  before_action :require_admin, except: %i[ show ]
   before_action :set_book, only: %i[ show edit update destroy ]
   skip_before_action :verify_authenticity_token, only: [:search_cover, :apply_cover]
 
